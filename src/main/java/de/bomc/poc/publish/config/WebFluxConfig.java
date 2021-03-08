03474281b91a1eba9d68e8602dc6d81ed33da6ce
bomc <michael_boerner@t-online.de>
2021-03-08 11:12:49 +0100

    delete EnableWebFlux annotation

--- src/main/java/de/bomc/poc/publish/config/WebFluxConfig.java
+++ src/main/java/de/bomc/poc/publish/config/WebFluxConfig.java
@@ -3,11 +3,9 @@ package de.bomc.poc.publish.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-//@EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer {
 
 	@Override
